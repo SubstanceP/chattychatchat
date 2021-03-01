19f6ac1b74185ef93c0cf9ee6178cd4ef55a701c
Peter Buddemeyer <peter.buddemeyer@gmail.com>
2021-02-28 23:02:15 -0800

    vercel

--- src/config/pubnub-keys.ts
+++ src/config/pubnub-keys.ts
@@ -1,4 +1,8 @@
 export default {
-  publishKey: process.env.REACT_APP_PUBLISH_KEY || "",
-  subscribeKey: process.env.REACT_APP_SUBSCRIBE_KEY || ""
+  publishKey:
+    process.env.REACT_APP_PUBLISH_KEY ||
+    "pub-c-38b62f6d-5744-401d-806c-4cebfca6cd58",
+  subscribeKey:
+    process.env.REACT_APP_SUBSCRIBE_KEY ||
+    "sub-c-a44f4598-77ce-11eb-a746-f25696a1d0fc"
 };
